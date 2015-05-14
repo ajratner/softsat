d6fb14e9503ee5f5a00fdb4e96c7df9b1e55777a
Daniel Selsam <daniel.selsam@gmail.com>
2015-05-13 18:50:26 -0700

    cleaned up Main

--- src/softsat/main/Main.java
+++ src/softsat/main/Main.java
@@ -8,13 +8,6 @@ import softsat.generate.BGMCSatData;
  */
 public class Main {
   
-  private static void demoDan() {
-    Variable var = new Variable(1,2);
-    Literal lit = new Literal(var,true);
-    System.out.println("Lit(Var(1,2),true)");
-    System.out.println(lit);
-  }
-
   private static void sanityBGMCSatData() {
     // BGMCSatData(int nClusters, int n, int k, double alpha, int numSoftClauses, int clusterNodesPerSoftClause, double softWeightMean, double softWeightStd) {
     BGMCSatData data = new BGMCSatData(3,4,2,2.0,2,2,10,1);
@@ -22,11 +15,6 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    demoDan();
     sanityBGMCSatData();
-    
-    //    HardClusters dataset = new HardClusters();
-    //    dataset.generate(3, 3, 3, 4.0, 0.5, 1.0, 0.1, 1);
-    //    dataset.print();
   }
 }
