212f163d88bf87384f730eed37075c7ba2971a31
Daniel Selsam <daniel.selsam@gmail.com>
2015-05-13 17:13:54 -0700

    junit not working

--- src/softsat/objects/VariableTest.java
+++ src/softsat/objects/VariableTest.java
@@ -7,7 +7,7 @@ public class VariableTest {
    @Test
    public void test_toString() {
      Variable var = new Variable(1,1);
-     assertTrue(var.toString().equals("<1:1>"));
+     assertEquals(var.toString(),"<1:1>");
    }
 
 }
